55c839a28c13ae59da282f2171f4cf2f44bdf6c2
bobraozal86 <Bobraozal86@gmail.com>
2022-11-03 18:24:54 +0700

    Pembuatan_API

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+using API.Context;
+using API.Handlers;
+using API.Repository.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private AccountRepository accountRepository;
+        public AccountController(AccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult Login(string email, string password)
+        {
+            try
+            {
+                var data = accountRepository.Login(email, password);
+                if(data != null)
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Data Not Found"
+                    });
+                }
+                else
+                {
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Data Found",
+                        Data = data
+                    });
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new
+                {
+                    StatusCode = 400,
+                    Message = ex.Message
+                });
+            }
+        }
+
+    }
+}
---
+++ Repository/Data/AccountRepository.cs
@@ -0,0 +1,28 @@
+using API.Context;
+using API.Handlers;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repository.Data
+{
+    public class AccountRepository
+    {
+        private MyContext myContext;
+        public AccountRepository(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+        public int Login (string email, string password)
+        {
+            var data = myContext.Users
+                .Include(x => x.Employee)
+                .Include(x => x.Role)
+                .SingleOrDefault(x => x.Employee.Email.Equals(email));
+            if (data != null && Hashing.ValidatePassword(password, data.Password) == true)
+            {         
+                var result = myContext.SaveChanges();
+                return result;
+            }
+            return 0;
+        }
+    }
+}
